d9470c9795d5e58564295d9075927a22fa2e9dfd
DaRealMiniToon <64669652+DaRealMiniToon@users.noreply.github.com>
2020-06-19 11:56:19 -0400

    new!
    ooh

--- src/bot.js
+++ src/bot.js
@@ -4,7 +4,7 @@ const discord = require('discord.js')
 const client = new discord.Client({ partials: ['MESSAGE', 'REACTION'] })
 const { registerCommands, registerEvents } = require('./utils/registry');
 (async () => {
-  client.login('NzE1MzQ5ODk3NDM5ODcxMDc4.XuzHjQ.TAA_y7n7UjBoPs_197b0PKMAOXk')
+  client.login(process.env.BOT_TOKEN);
   client.commands = new Map()
   client.cachedMessageReactions = new Map()
   await registerEvents(client, '../events')
